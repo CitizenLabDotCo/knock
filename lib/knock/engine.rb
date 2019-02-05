2a73aac587e3358a701b731ae0fd1fb2660f8569
sebastienhoorens <31925816+sebastienhoorens@users.noreply.github.com>
2019-02-05 10:29:53 +0100

    Don't load everything to prevent Generators::Base load error

--- lib/knock/engine.rb
+++ lib/knock/engine.rb
@@ -1,6 +1,14 @@
 module Knock
   class Engine < ::Rails::Engine
-    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
+    # After https://github.com/ngty/knock, commit 97fb32f.
+    # This prevents the error "Unable to load application: 
+    # NameError: uninitialized constant Generators::Base"
+    # upon build.
+    if ::Rails.const_defined? 'Generators'
+      config.eager_load_paths += Dir["#{config.root}/lib/**/"]
+    else
+      require_relative './authenticable'
+    end
     isolate_namespace Knock
   end
 end
